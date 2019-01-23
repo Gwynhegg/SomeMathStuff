1b6c2b53ca1e35db89105e14cfc733934fc284e4
Gwynhegg <mister.chepurov24@gmail.com>
2019-01-23 21:33:24 +0500

    Добавьте файлы проекта.

---
+++ SomeMathStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeMathStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
